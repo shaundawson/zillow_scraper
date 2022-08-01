d716c13cdb4904849a89c55e28cf8c4d32e081b0
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-07-31 21:28:26 -0400

    scrape zillow listings

---
+++ main.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import lxml
+import requests
+import smtplib
+
+
+zillow_url='https://www.zillow.com/homes/for_rent/2-_beds/?searchQueryState={"usersSearchTerm":"Brooklyn, New York, NY","mapBounds":{"west":-74.07916397460939,"east":-73.81755203613282,"south":40.564209391228374,"north":40.74627045249812},"isMapVisible":true,"filterState":{"fsba":{"value":false},"fsbo":{"value":false},"nc":{"value":false},"fore":{"value":false},"cmsn":{"value":false},"auc":{"value":false},"fr":{"value":true},"ah":{"value":true},"beds":{"min":2},"mp":{"max":2300},"price":{"max":551266}},"isListVisible":true,"mapZoom":12,"regionSelection":[],"customRegionId":"0c64262e72X1-CR1s8vqrvzqflq_wb2hl"}'
+
+
+headers={
+    "Accept-Language":"en-US,en;q=0.9",
+    "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+    }
+
+# Scrape Zillow
+response = requests.get(zillow_url,headers=headers)
+soup = BeautifulSoup(response.text, "html.parser")
+search_results = soup.find_all(name="div", class_="list-card-info")
+
+for result in search_results:
+    listing_price = result.getText().strip()
+    print(listing_price)
